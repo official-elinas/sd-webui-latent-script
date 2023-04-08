45245941ce17e1e9735f4b776a5bb66e83dc5e4a
Elinas <support@sneerz.com>
2023-04-07 19:35:42 -0500

    add option to disable mean for weights

--- scripts/latent_patch.py
+++ scripts/latent_patch.py
@@ -1,8 +1,8 @@
 import torch
-import modules.scripts as scripts
 import gradio as gr
-
+import modules.scripts as scripts
 from modules import devices
+from modules.sd_hijack_clip import FrozenCLIPEmbedderWithCustomWordsBase
 from modules.scripts import AlwaysVisible
 
 
@@ -12,19 +12,20 @@ class Script(scripts.Script):
         super().__init__(**kwargs)
         self.default_randn = devices.randn
         self.default_randn_without_seed = devices.randn_without_seed
+        self.default_sd_hijack_clip_process_tokens = FrozenCLIPEmbedderWithCustomWordsBase.process_tokens
 
     def title(self):
-        return "Latent Script"
+        return "Latent Modifier"
 
     def show(self, is_img2img):
         return AlwaysVisible
 
     def ui(self, is_img2img):
         is_cpu_seed = gr.Checkbox(False, label="Generate Latent (Seed) on CPU for more consistent results")
-        # is_mean_disabled = gr.Checkbox(False, label="Disable mean calculation to make weights identical")
-        return [is_cpu_seed]
+        is_mean_disabled = gr.Checkbox(False, label="Disable mean calculation to make weights identical")
+        return [is_cpu_seed, is_mean_disabled]
 
-    def process(self, p, is_cpu_seed):
+    def process(self, p, is_cpu_seed, is_mean_disabled):
 
         if is_cpu_seed:
             print("Using CPU seed")
@@ -43,3 +44,19 @@ class Script(scripts.Script):
             # restore
             devices.randn = self.default_randn
             devices.randn_without_seed = self.default_randn_without_seed
+
+        if is_mean_disabled:
+            def bypass_process_tokens(self, remade_batch_tokens, batch_multipliers):
+                tokens = torch.asarray(remade_batch_tokens).to(devices.device)
+                if self.id_end != self.id_pad:
+                    for batch_pos in range(len(remade_batch_tokens)):
+                        index = remade_batch_tokens[batch_pos].index(self.id_end)
+                        tokens[batch_pos, index + 1:tokens.shape[1]] = self.id_pad
+                z = self.encode_with_transformers(tokens)
+                batch_multipliers = torch.asarray(batch_multipliers).to(devices.device)
+                z = z * batch_multipliers.reshape(batch_multipliers.shape + (1,)).expand(z.shape)
+                return z
+
+            FrozenCLIPEmbedderWithCustomWordsBase.process_tokens = bypass_process_tokens
+        else:
+            FrozenCLIPEmbedderWithCustomWordsBase.process_tokens = self.default_sd_hijack_clip_process_tokens
